d10211184520240e4b21e19413f380f2ac662523
Dileep Yadav <dy695354612@gmail.com>
2026-09-01 21:48:27 +0530

    express

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ let port = 3000;
 app.listen(port, () => {
     console.log(`app is listening on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+    res.send("you contacted root path");
+});
+
+app.get("/apple", (req, res) => {
+    res.send("you contacted apple path");
+});
+
+app.get("/orange", (req, res) => {
+    res.send("you contacted orange path");
+});
+
+app.post("/", (req, res) => {
+    res.send("hello, i am root");
+});
+
+
+
+// app.use((req, res) => {
+//     console.log("request received");
+//     res.send("this is a basic response");
+// });
